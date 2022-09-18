7f5272b7977be7c73257227e2d41c96f72a44bbb
Carlos Carrillo <90294685+cjst3ch@users.noreply.github.com>
2022-09-18 18:43:44 -0500

    rspec testing issues

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe 'Creating a book', type: :feature do
     visit new_book_path
     fill_in 'Title', with: 'Lord of the Rings'
     fill_in 'Author', with: 'J.R.R. Tolkein'
-    fill_in 'Price', with: '10.56'
-    fill_in 'Date Published', with: '0000-00-00'
+    fill_in 'Price', with: 10.56
+    fill_in 'Date', with: '2020-09-28'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('Lord of the Rings')
     expect(page).to have_content('J.R.R. Tolkein')
-    expect(page).to have_content('10.56')
-    expect(page).to have_content('0000-00-00')
+    expect(page).to have_content(10.56)
+    expect(page).to have_content('2020-09-28')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'Lord of the Rings', author: 'J.R.R. Tolkien', price: 10.23, date: '0000-00-00')
+    described_class.new(title: 'Lord of the Rings', author: 'J.R.R. Tolkien', price: 10.23, date: '2020-09-28')
   end
 
   it 'is valid with all valid attributes' do
@@ -15,17 +15,18 @@ RSpec.describe Book, type: :model do
     expect(subject).not_to be_valid
   end
 
-  it 'is not valid without a title' do
+  it 'is not valid without an author' do
     subject.author = nil
     expect(subject).not_to be_valid
   end
 
-  it 'is not valid without a title' do
+  it 'is not valid without a price' do
     subject.price = nil
     expect(subject).not_to be_valid
   end
 
-  it 'is not valid without a title' do
+  it 'is not valid without a date' do
     subject.date = nil
     expect(subject).not_to be_valid
   end
+end
